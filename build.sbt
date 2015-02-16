adc1b9559c18bdba36caa0eeac514368fa274f7b
evdokim <ekovach@ohnosequences.com>
2015-02-16 13:11:36 +0100

    add test aws credentials

--- build.sbt
+++ build.sbt
@@ -13,11 +13,12 @@ scalaVersion := "2.10.4"
 crossScalaVersions := Seq("2.11.4")
 
 libraryDependencies ++= Seq(
-//  "com.amazonaws" % "aws-java-sns" % "1.9.8",
-//  "com.amazonaws" % "aws-java-sqs" % "1.9.8",
-//  "com.amazonaws" % "aws-java-s3" % "1.9.8",
-//  "com.amazonaws" % "aws-java-ec2" % "1.9.8",
-//  "com.amazonaws" % "aws-java-iam" % "1.9.8",
-  "com.amazonaws" % "aws-java-sdk" % "1.8.11",
+  "com.amazonaws" % "aws-java-sdk-sns" % "1.9.19",
+  "com.amazonaws" % "aws-java-sdk-sqs" % "1.9.19",
+  "com.amazonaws" % "aws-java-sdk-s3" % "1.9.19",
+  "com.amazonaws" % "aws-java-sdk-ec2" % "1.9.19",
+  "com.amazonaws" % "aws-java-sdk-iam" % "1.9.19",
+  "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.9.19",
+  "com.amazonaws" % "aws-java-sdk-autoscaling" % "1.9.19",
   "com.novocode" % "junit-interface" % "0.11" % "test"
 )
---
+++ project/Build.scala
@@ -0,0 +1,47 @@
+import sbt._
+import Keys._
+import com.amazonaws.auth._
+
+object AWSToolsBuild extends Build {
+
+  val testCredentialsProvider = SettingKey[Option[AWSCredentialsProvider]]("credentials provider for test environment")
+
+  override lazy val settings = super.settings ++ Seq(testCredentialsProvider := None)
+
+  def providerConstructorPrinter(provider: AWSCredentialsProvider) = provider match {
+    case ip: InstanceProfileCredentialsProvider => {
+      "new com.amazonaws.auth.InstanceProfileCredentialsProvider()"
+    }
+    case ep: EnvironmentVariableCredentialsProvider => {
+      "new com.amazonaws.auth.EnvironmentVariableCredentialsProvider()"
+    }
+    case pp: PropertiesFileCredentialsProvider => {
+      val field = pp.getClass().getDeclaredField("credentialsFilePath")
+      field.setAccessible(true)
+      val path = field.get(pp).toString
+      "new com.amazonaws.auth.PropertiesFileCredentialsProvider(\"\"\"$path$\"\"\")".replace("$path$", path)
+    }
+
+    //todo fix!
+    case p => ""
+  }
+
+  lazy val root = Project(id = "aws",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      sourceGenerators in Compile += task[Seq[File]] {
+        val text = """
+                     |package generated.test
+                     |
+                     |object credentials {
+                     |  val credentialsProvider: Option[com.amazonaws.auth.AWSCredentialsProvider] = $cred$
+                     |}
+                     |""".stripMargin
+          .replace("$cred$", testCredentialsProvider.value.map(providerConstructorPrinter).toString)
+        val file = (sourceManaged in Compile).value / "testCredentials.scala"
+        IO.write(file, text)
+        Seq(file)
+      }
+    )
+  )
+}
