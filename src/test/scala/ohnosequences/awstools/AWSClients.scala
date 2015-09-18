589ab4431b7d17c4de14f24280249688e77058c4
Alexey Alekhin <laughedelic@gmail.com>
2015-09-18 13:38:48 +0200

    Changed test credentials provider to the default chain; let's see if it fixes travis

--- src/test/scala/ohnosequences/awstools/AWSClients.scala
+++ src/test/scala/ohnosequences/awstools/AWSClients.scala
@@ -4,5 +4,5 @@ import com.amazonaws.auth._, profile._
 
 case object test {
 
-  lazy val awsClients: AWSClients = AWSClients.create(new ProfileCredentialsProvider("default"))
+  lazy val awsClients: AWSClients = AWSClients.create(new DefaultAWSCredentialsProviderChain())
 }
