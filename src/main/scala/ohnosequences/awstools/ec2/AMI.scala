6f4c7b775be6ffcc9e28469a9a0e20f3b9723508
Alexey Alekhin <laughedelic@gmail.com>
2015-10-26 15:57:11 +0100

    Added type members for AMI parameters

--- src/main/scala/ohnosequences/awstools/ec2/AMI.scala
+++ src/main/scala/ohnosequences/awstools/ec2/AMI.scala
@@ -1,6 +1,6 @@
 package ohnosequences.awstools.ec2
 
-import ohnosequences.awstools.regions._, Region._
+import ohnosequences.awstools.regions.{ Region => AWSRegion, _ }, AWSRegion._
 
 /* ## [Amazon Machine Images (AMI)](http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ComponentsAMIs.html) */
 case object ami {
@@ -15,16 +15,16 @@ case object ami {
   sealed trait Virtualization
   /* All current generation instance types support HVM AMIs.
      The CC2, CR1, HI1, and HS1 previous generation instance types support HVM AMIs. */
-  case object HVM extends Virtualization
+  case object HVM extends Virtualization; type HVM = HVM.type
   /* The C3 and M3 current generation instance types support PV AMIs.
      The C1, HI1, HS1, M1, M2, and T1 previous generation instance types support PV AMIs. */
-  case object PV extends Virtualization
+  case object PV extends Virtualization; type PV = PV.type
 
 
   /* ### [Storage for the Root Device](http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ComponentsAMIs.html#storage-for-the-root-device) */
   sealed trait StorageType
-  case object EBS extends StorageType
-  case object InstanceStore extends StorageType
+  case object EBS extends StorageType; type EBS = EBS.type
+  case object InstanceStore extends StorageType; type InstanceStore = InstanceStore.type
 
 
   // TODO: [Launch permissions](http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ComponentsAMIs.html#launch-permissions)
@@ -36,10 +36,17 @@ case object ami {
     val id: String
     val version: String
 
-    val region: Region
-    val architecture: Architecture
-    val virtualization: Virtualization
-    val storageType: StorageType
+    type Region <: AWSRegion
+    val  region: Region
+
+    type Arch <: Architecture
+    val  arch: Arch
+
+    type Virt <: Virtualization
+    val  virt: Virt
+
+    type Storage <: StorageType
+    val  storage: Storage
   }
 
   // Amazon Linux AMI 2015.09
@@ -47,128 +54,144 @@ case object ami {
   trait AnyAmazonLinuxAMI extends AnyLinuxAMI {
 
     final val version: String = "2015.09"
-    final val architecture: Architecture = x86_64
+
+    type Arch = x86_64.type
+    final val arch: Arch = x86_64
 
     final lazy val id: String = s"ami-${idNum}"
 
-    private val idNum: String = region match {
-      case NorthernVirginia  => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "e3106686"
-          case InstanceStore => "65116700"
+    private lazy val idNum: String = {
+      val r: AWSRegion = region
+      val v: Virtualization = virt
+      val s: StorageType = storage
+
+      r match {
+        case NorthernVirginia  => v match {
+          case HVM => s match {
+            case EBS           => "e3106686"
+            case InstanceStore => "65116700"
+          }
+          case PV  => s match {
+            case EBS           => "cf1066aa"
+            case InstanceStore => "971066f2"
+          }
         }
-        case PV  => storageType match {
-          case EBS           => "cf1066aa"
-          case InstanceStore => "971066f2"
+        case Oregon => v match {
+          case HVM => s match {
+            case EBS           => "9ff7e8af"
+            case InstanceStore => "bbf7e88b"
+          }
+          case PV  => s match {
+            case EBS           => "81f7e8b1"
+            case InstanceStore => "bdf7e88d"
+          }
         }
-      }
-      case Oregon => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "9ff7e8af"
-          case InstanceStore => "bbf7e88b"
+        case NorthernCalifornia => v match {
+          case HVM => s match {
+            case EBS           => "cd3aff89"
+            case InstanceStore => "d53aff91"
+          }
+          case PV  => s match {
+            case EBS           => "d53aff91"
+            case InstanceStore => "c93aff8d"
+          }
         }
-        case PV  => storageType match {
-          case EBS           => "81f7e8b1"
-          case InstanceStore => "bdf7e88d"
+        case Ireland => v match {
+          case HVM => s match {
+            case EBS           => "69b9941e"
+            case InstanceStore => "7db9940a"
+          }
+          case PV  => s match {
+            case EBS           => "a3be93d4"
+            case InstanceStore => "8fbe93f8"
+          }
         }
-      }
-      case NorthernCalifornia => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "cd3aff89"
-          case InstanceStore => "d53aff91"
+        case Frankfurt => v match {
+          case HVM => s match {
+            case EBS           => "daaeaec7"
+            case InstanceStore => "a2aeaebf"
+          }
+          case PV  => s match {
+            case EBS           => "a6aeaebb"
+            case InstanceStore => "a0aeaebd"
+          }
         }
-        case PV  => storageType match {
-          case EBS           => "d53aff91"
-          case InstanceStore => "c93aff8d"
+        case Singapore => v match {
+          case HVM => s match {
+            case EBS           => "52978200"
+            case InstanceStore => "ac9481fe"
+          }
+          case PV  => s match {
+            case EBS           => "50978202"
+            case InstanceStore => "4c97821e"
+          }
         }
-      }
-      case Ireland => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "69b9941e"
-          case InstanceStore => "7db9940a"
+        case Tokyo => v match {
+          case HVM => s match {
+            case EBS           => "9a2fb89a"
+            case InstanceStore => "a22fb8a2"
+          }
+          case PV  => s match {
+            case EBS           => "9c2fb89c"
+            case InstanceStore => "a42fb8a4"
+          }
         }
-        case PV  => storageType match {
-          case EBS           => "a3be93d4"
-          case InstanceStore => "8fbe93f8"
+        case Sydney => v match {
+          case HVM => s match {
+            case EBS           => "c11856fb"
+            case InstanceStore => "871856bd"
+          }
+          case PV  => s match {
+            case EBS           => "c71856fd"
+            case InstanceStore => "851856bf"
+          }
         }
-      }
-      case Frankfurt => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "daaeaec7"
-          case InstanceStore => "a2aeaebf"
+          case SaoPaulo => v match {
+          case HVM => s match {
+            case EBS           => "3b0c9926"
+            case InstanceStore => "030c991e"
+          }
+          case PV  => s match {
+            case EBS           => "370c992a"
+            case InstanceStore => "010c991c"
+          }
         }
-        case PV  => storageType match {
-          case EBS           => "a6aeaebb"
-          case InstanceStore => "a0aeaebd"
+        case Beijing => v match {
+          case HVM => s match {
+            case EBS           => "6cb22e55"
+            case InstanceStore => "76b22e4f"
+          }
+          case PV  => s match {
+            case EBS           => "54b22e6d"
+            case InstanceStore => "68b22e51"
+          }
         }
-      }
-      case Singapore => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "52978200"
-          case InstanceStore => "ac9481fe"
-        }
-        case PV  => storageType match {
-          case EBS           => "50978202"
-          case InstanceStore => "4c97821e"
-        }
-      }
-      case Tokyo => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "9a2fb89a"
-          case InstanceStore => "a22fb8a2"
-        }
-        case PV  => storageType match {
-          case EBS           => "9c2fb89c"
-          case InstanceStore => "a42fb8a4"
-        }
-      }
-      case Sydney => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "c11856fb"
-          case InstanceStore => "871856bd"
-        }
-        case PV  => storageType match {
-          case EBS           => "c71856fd"
-          case InstanceStore => "851856bf"
-        }
-      }
-        case SaoPaulo => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "3b0c9926"
-          case InstanceStore => "030c991e"
-        }
-        case PV  => storageType match {
-          case EBS           => "370c992a"
-          case InstanceStore => "010c991c"
-        }
-      }
-      case Beijing => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "6cb22e55"
-          case InstanceStore => "76b22e4f"
-        }
-        case PV  => storageType match {
-          case EBS           => "54b22e6d"
-          case InstanceStore => "68b22e51"
-        }
-      }
-      case GovCloud => virtualization match {
-        case HVM => storageType match {
-          case EBS           => "ad34568e"
-          case InstanceStore => "a5345686"
-        }
-        case PV  => storageType match {
-          case EBS           => "b3345690"
-          case InstanceStore => "ab345688"
+        case GovCloud => v match {
+          case HVM => s match {
+            case EBS           => "ad34568e"
+            case InstanceStore => "a5345686"
+          }
+          case PV  => s match {
+            case EBS           => "b3345690"
+            case InstanceStore => "ab345688"
+          }
         }
       }
     }
   }
 
-  case class AmazonLinuxAMI(
-    val region: Region,
-    val virtualization: Virtualization,
-    val storageType: StorageType
-  ) extends AnyAmazonLinuxAMI
+  case class AmazonLinuxAMI[
+    R <: AWSRegion,
+    V <: Virtualization,
+    S <: StorageType
+  ](val region: R,
+    val virt: V,
+    val storage: S
+  ) extends AnyAmazonLinuxAMI {
+
+    type Region = R
+    type Virt = V
+    type Storage = S
+  }
 
 }
