5101de676b0dafabe3842db975a296684569d0ef
Alexey Alekhin <laughedelic@gmail.com>
2016-03-10 16:39:59 +0100

    Removed AWSClients from the tests code; added a note about it

--- src/test/scala/ohnosequences/awstools/AWSClients.scala
+++
@@ -1,8 +0,0 @@
-package ohnosequences.awstools
-
-import com.amazonaws.auth._, profile._
-
-case object test {
-
-  lazy val awsClients: AWSClients = AWSClients.create(new DefaultAWSCredentialsProviderChain())
-}
