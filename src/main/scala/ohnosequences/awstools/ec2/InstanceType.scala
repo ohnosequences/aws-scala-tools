9b1bead92ae31e14508bd13b878b508541850508
Alexey Alekhin <laughedelic@gmail.com>
2016-10-26 17:52:52 +0200

    Added missing instance types

--- src/main/scala/ohnosequences/awstools/ec2/InstanceType.scala
+++ src/main/scala/ohnosequences/awstools/ec2/InstanceType.scala
@@ -19,7 +19,7 @@ case object AnyInstanceType {
   type ofFamily[F <: Family] = AnyInstanceType { type Family = F }
 
   implicit def toJavaInstanceType(t: AnyInstanceType):
-    model.InstanceType = 
+    model.InstanceType =
     model.InstanceType.fromValue(t.name)
 }
 
@@ -38,12 +38,13 @@ case object InstanceType {
   trait CurrentGeneration extends AnyGeneration
   trait PreviousGeneration extends AnyGeneration
 
-  sealed class Family(val prefix: String)
+  sealed class Family(val prefix: String) { family: AnyGeneration => }
 
-  // Current Generation Instances //
+  /* ### Current Generation Instances */
 
-  // General purpose
+  /* #### General purpose */
   case object t2 extends Family("t2") with CurrentGeneration {
+    case object nano   extends InstanceType(t2, "nano")
     case object micro  extends InstanceType(t2, "micro")
     case object small  extends InstanceType(t2, "small")
     case object medium extends InstanceType(t2, "medium")
@@ -55,6 +56,7 @@ case object InstanceType {
     case object x2large  extends InstanceType(m4, "2xlarge")
     case object x4large  extends InstanceType(m4, "4xlarge")
     case object x10large extends InstanceType(m4, "10xlarge")
+    case object x16large extends InstanceType(m4, "16xlarge")
   }
   case object m3 extends Family("m3") with CurrentGeneration {
     case object medium  extends InstanceType(m3, "medium")
@@ -63,7 +65,7 @@ case object InstanceType {
     case object x2large extends InstanceType(m3, "2xlarge")
   }
 
-  // Compute optimized
+  /* #### Compute optimized */
   case object c4 extends Family("c4") with CurrentGeneration {
     case object large   extends InstanceType(c4, "large")
     case object xlarge  extends InstanceType(c4, "xlarge")
@@ -79,7 +81,7 @@ case object InstanceType {
     case object x8large extends InstanceType(c3, "8xlarge")
   }
 
-  // Memory optimized
+  /* #### Memory optimized */
   case object r3 extends Family("r3") with CurrentGeneration {
     case object large   extends InstanceType(r3, "large")
     case object xlarge  extends InstanceType(r3, "xlarge")
@@ -87,15 +89,18 @@ case object InstanceType {
     case object x4large extends InstanceType(r3, "4xlarge")
     case object x8large extends InstanceType(r3, "8xlarge")
   }
+  case object x1 extends Family("x1") with CurrentGeneration {
+    case object x16large extends InstanceType(x1, "16xlarge")
+    case object x32large extends InstanceType(x1, "32xlarge")
+  }
 
-  // Storage optimized
+  /* #### Storage optimized */
   case object i2 extends Family("i2") with CurrentGeneration {
     case object xlarge  extends InstanceType(i2, "xlarge")
     case object x2large extends InstanceType(i2, "2xlarge")
     case object x4large extends InstanceType(i2, "4xlarge")
     case object x8large extends InstanceType(i2, "8xlarge")
   }
-
   case object d2 extends Family("d2") with CurrentGeneration {
     case object xlarge  extends InstanceType(d2, "xlarge")
     case object x2large extends InstanceType(d2, "2xlarge")
@@ -103,10 +108,21 @@ case object InstanceType {
     case object x8large extends InstanceType(d2, "8xlarge")
   }
 
+  /* #### Accelerated computing */
+  case object p2 extends Family("p2") with CurrentGeneration {
+    case object xlarge   extends InstanceType(p2, "xlarge")
+    case object x8large  extends InstanceType(p2, "8xlarge")
+    case object x16large extends InstanceType(p2, "16xlarge")
+  }
+  case object g2 extends Family("g2") with CurrentGeneration {
+    case object x2large extends InstanceType(g2, "2xlarge")
+    case object x8large extends InstanceType(g2, "8xlarge")
+  }
 
-  // Previous Generation Instances //
 
-  // General purpose
+  /* ### Previous Generation Instances */
+
+  /* #### General purpose */
   case object m1 extends Family("m1") with PreviousGeneration {
     case object small  extends InstanceType(m1, "small")
     case object medium extends InstanceType(m1, "medium")
@@ -114,7 +130,7 @@ case object InstanceType {
     case object xlarge extends InstanceType(m1, "xlarge")
   }
 
-  // Compute optimized
+  /* #### Compute optimized */
   case object c1 extends Family("c1") with PreviousGeneration {
     case object medium  extends InstanceType(c1, "medium")
     case object xlarge  extends InstanceType(c1, "xlarge")
@@ -123,7 +139,7 @@ case object InstanceType {
     case object x8large extends InstanceType(cc2, "8xlarge")
   }
 
-  // Memory optimized
+  /* #### Memory optimized */
   case object m2 extends Family("m2") with PreviousGeneration {
     case object xlarge  extends InstanceType(m2, "xlarge")
     case object x2large extends InstanceType(m2, "2xlarge")
@@ -133,7 +149,7 @@ case object InstanceType {
     case object x8large extends InstanceType(cr1, "8xlarge")
   }
 
-  // Storage optimized
+  /* #### Storage optimized */
   case object hi1 extends Family("hi1") with PreviousGeneration {
     case object x4large extends InstanceType(hi1, "4xlarge")
   }
@@ -141,12 +157,12 @@ case object InstanceType {
     case object x8large extends InstanceType(hs1, "8xlarge")
   }
 
-  // GPU instances
+  /* #### GPU instances */
   case object cg1 extends Family("cg1") with PreviousGeneration {
     case object x4large extends InstanceType(cg1, "4xlarge")
   }
 
-  // Micro instances
+  /* #### Micro instances */
   case object t1 extends Family("t1") with PreviousGeneration {
     case object micro extends InstanceType(t1, "micro")
   }
