e0dbdcb0e665ed8a1cd00d2865f6f3ded14e10cb
Alexey Alekhin <laughedelic@gmail.com>
2016-10-26 15:38:36 +0200

    Removed dead test code

--- src/test/scala/ohnosequences/awstools/autoscaling.scala
+++ src/test/scala/ohnosequences/awstools/autoscaling.scala
@@ -7,10 +7,7 @@ import com.amazonaws.services.autoscaling.AmazonAutoScaling
 
 class AutoScaling extends org.scalatest.FunSuite with org.scalatest.BeforeAndAfterAll {
 
-  lazy val autoscaling: AmazonAutoScaling = AutoScalingClient(
-    Region.Ireland,
-    new DefaultAWSCredentialsProviderChain()
-  )
+  lazy val autoscaling: AmazonAutoScaling = AutoScalingClient()
 
 
   override def beforeAll() = {
